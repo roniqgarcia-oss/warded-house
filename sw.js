5b0644903cb98ede79b25790046e72bf70dd9cec
roniqgarcia-oss <roniqgarcia@gmail.com>
2026-04-30 14:42:15 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // The Warded House — Service Worker v1777473520
-const CACHE = 'warded-house-1777488866';
+const CACHE = 'warded-house-1777531202';
 const SHELL = ['/warded-house/', '/warded-house/index.html'];
 
 self.addEventListener('install', e => {
